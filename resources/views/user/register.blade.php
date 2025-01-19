029603c1330b99d61a32bd4e052939ceb7e5130b
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-20 00:11:36 +0330

    create login and register panel for user

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>register</title>
+    <title>user register</title>
     @vite('resources/css/app.css')
 </head>
 <body>
 <div class="w-svw h-svh flex justify-center items-center bg-gray-200">
     <div class="w-2/6 h-5/6 bg-white rounded-xl flex flex-col items-center justify-start">
         <h1 class="text-3xl pt-5">ثبت نام</h1>
-        <form action="{{route('register')}}" method="post" class="w-5/6 flex flex-col items-end justify-start">
+        <form action=" " method="post" class="w-5/6 flex flex-col items-end justify-start">
             @csrf
             <label for="name" class="mt-2">نام</label>
             <input type="text" name="name" id="name" class="w-full h-10 bg-gray-100 rounded mt-5">
